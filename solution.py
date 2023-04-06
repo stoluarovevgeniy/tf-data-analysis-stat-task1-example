4e008f68519d2caecb307d49cd09d444a31be3cd
stoluarovevgeniy <130039269+stoluarovevgeniy@users.noreply.github.com>
2023-04-06 15:17:51 +0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 2049733859 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     return np.log(np.median(x))
